aa867fe8ec4cce3156fb16743723b0df846ec09b
AStaege <andreas.staege@googlemail.com>
2017-05-19 12:36:58 +0200

    some advancement

--- framework7/js/my-app.js
+++ framework7/js/my-app.js
@@ -121,8 +121,8 @@ function quaggerei (){
             inputStream: {
                 type : "LiveStream",
                 constraints: {
-                    width: {min: 800},
-                    height: {min: 600},
+                    width: {min: 1280},
+                    height: {min: 720},
                     facingMode: "environment",
                     aspectRatio: {min: 1, max: 2}
                 }
