b3811f1b01f3144cbcdd3128a5e68ed220ce7493
AStaege <andreas.staege@googlemail.com>
2017-05-19 09:19:59 +0200

    Initial Commit

--- framework7/js/my-app.js
+++ framework7/js/my-app.js
@@ -112,40 +112,37 @@ function quaggerei (){
             numOfWorkers: 4,
             decoder: {
                 readers : [{
-                    format: "code_128_reader",
+                    format: "ean_reader",
                     config: {}
                 }]
             },
             locate: true
         },
-        /* state: {
-            inputStream: {
-                type : "LiveStream",
-                constraints: {
-                    width: {min: 800},
-                    height: {min: 600},
-                    facingMode: "environment",
-                    aspectRatio: {min: 1, max: 2}
-                }
-            },
-            locator: {
-                patchSize: "medium",
-                halfSample: true
-            },
-            numOfWorkers: 2,
-            decoder: {
-                readers : [{
-                    format: "ean_reader",
-                    config: {}
-                }, {
-                    format: "code_39_reader",
-                    config: {}
-                }]
-            },
-            locate: true
-        },*/
         lastResult: null
     };
+    Quagga.onProcessed(function(result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+            drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+            if (result.boxes) {
+                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                result.boxes.filter(function (box) {
+                    return box !== result.box;
+                }).forEach(function (box) {
+                    Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+                });
+            }
+
+            if (result.box) {
+                Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+            }
+
+            if (result.codeResult && result.codeResult.code) {
+                Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+            }
+        }
+    });
     App.init();
 }
 
