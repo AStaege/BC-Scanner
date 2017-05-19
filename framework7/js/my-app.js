6b520a1bdab7655a4a0eb9589c9591600db09071
AStaege <andreas.staege@googlemail.com>
2017-05-19 10:37:24 +0200

    Initial Commit

--- framework7/js/my-app.js
+++ framework7/js/my-app.js
@@ -168,6 +168,21 @@ function quaggerei (){
             }
         }
     });
+    Quagga.onDetected(function(result) {
+        var code = result.codeResult.code;
+        console.log("code: " + code);
+/*
+        if (App.lastResult !== code) {
+            App.lastResult = code;
+            var $node = null, canvas = Quagga.canvas.dom.image;
+
+            $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
+            $node.find("img").attr("src", canvas.toDataURL());
+            $node.find("h4.code").html(code);
+            $("#result_strip ul.thumbnails").prepend($node);
+        }
+        */
+    });
     App.init();
 }
 
